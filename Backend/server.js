6425e3dec85fd45d71b1fb9b333220b489c2fb82
lakshitkumarsinghlks02 <lakshitkumarsingh.lks02@gmail.com>
2026-02-26 02:39:56 +0530

    Updated booking email template in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ const sendBookingEmail = async (bookingData) => {
                 <p><strong>Email:</strong> ${bookingData.email}</p>
                 <p><strong>Check-in:</strong> ${bookingData.checkIn}</p>
                 <p><strong>Check-out:</strong> ${bookingData.checkOut}</p>
-                <p><strong>Check-guests:</strong> ${bookingData.guests}</p>
+                <p><strong>Guests:</strong> ${bookingData.guests}</p>
                 <hr/>
                 <p style="color:gray;">This booking was submitted from your hotel website.</p>
             </div>
