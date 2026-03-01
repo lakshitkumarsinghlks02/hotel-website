35222e5857f6b2cb399046a8cc2aba9af13eabe8
lakshitkumarsinghlks02 <lakshitkumarsingh.lks02@gmail.com>
2026-03-01 23:14:29 +0530

    Re-push current code without changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Booking from "./pages/Booking";
+import "./index.css";
 
 function App() {
   return (
